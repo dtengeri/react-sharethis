4fb4557812421103bd46144ea77cef1fb03bbc1f
dtengeri <dtengeri@gmail.com>
2015-11-24 20:36:23 +0100

    Added React ShareThis component.

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var React = require('react');
+
+var ShareThisButton = React.createClass({
+  propTypes: {
+    service: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired,
+    title: React.PropTypes.string,
+    type: React.PropTypes.string,
+  },
+
+  getDefaultProps: function getDefaultProps() {
+    return {
+      title: '',
+      type: 'large'
+    };
+  },
+
+  componentDidMount: function componentDidMount() {
+    stWidget.addEntry({
+      service: this.props.service,
+      element: this.refs.button,
+      url: this.props.url,
+      title: this.props.title,
+      type: this.props.type,
+    });
+  },
+
+  render: function render() {
+    var classes = ['share', 'share-' + this.props.service];
+    return (
+      <span className={classes.join(' ')} ref="button"></span>
+    );
+  },
+});
+
+module.exports = ShareThisButton;
